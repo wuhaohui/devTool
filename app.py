ab5fd4bb3989192d0969c3ef778b96aef3f11138
haohu <haohui.wu@outlook.com>
2022-05-19 18:07:34 +0800

    fix bug

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
+import re
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QGraphicsView, QPushButton, QHBoxLayout, QVBoxLayout, QMessageBox, \
-    QLabel
-from PyQt5.QtCore import Qt, QByteArray, QBuffer, QIODevice,QTimer
+    QLabel, QTextEdit
+from PyQt5.QtCore import Qt, QByteArray, QBuffer, QIODevice, QTimer
 from PyQt5.QtGui import QClipboard
 from tool import Tools
 from notice import NotificationWindow
@@ -20,10 +21,12 @@ class App(QWidget):
         self.resize(350, 200)
 
         label = QLabel(self)
-        label.setText("<a href='https://union-click.jd.com/jdc?e=618%7Cpc%7C&p=JF8BAKcJK1olXDYDZBoCUBVIMzZNXhpXVhgcEhgbFxBCHD1WR2tXJw5VSlc9byloACxWZSNvWmV7EwY9BEcnBm8JGlwRVAUGV25dCUoWAmsPE10dbTYCVW4WZkonAW4JHlgVWgAFV25dD08XC28IHVkTWQQBZFldAXsVAGoAGlMQVQ8eXFpYDEIXM184GGsSXQ8WUiwcWl8RcV84G1slXTZdEAMAOEkWAmsBKw'>支持</a>")
+        label.setText(
+            "<a href='https://union-click.jd.com/jdc?e=618%7Cpc%7C&p=JF8BAKcJK1olXDYDZBoCUBVIMzZNXhpXVhgcEhgbFxBCHD1WR2tXJw5VSlc9byloACxWZSNvWmV7EwY9BEcnBm8JGlwRVAUGV25dCUoWAmsPE10dbTYCVW4WZkonAW4JHlgVWgAFV25dD08XC28IHVkTWQQBZFldAXsVAGoAGlMQVQ8eXFpYDEIXM184GGsSXQ8WUiwcWl8RcV84G1slXTZdEAMAOEkWAmsBKw'>支持</a>")
         label.setOpenExternalLinks(True)
         label.setAlignment(Qt.AlignCenter)
 
+        self.ruleText = QTextEdit(self)
         oneButton = QPushButton("智能转换")
         oneButton.clicked.connect(self.samrtTrans)
 
@@ -31,12 +34,12 @@ class App(QWidget):
         twoButton.clicked.connect(self.filterQuotation)
 
         threeButton = QPushButton("测试")
-        threeButton.clicked.connect(lambda :self.aboutqt())
-
+        threeButton.clicked.connect(lambda: self.aboutqt())
 
         # 布局
         vbox = QVBoxLayout()
         vbox.addStretch(1)
+        vbox.addWidget(self.ruleText)
         vbox.addWidget(oneButton)
         vbox.addWidget(twoButton)
         vbox.addWidget(label)
@@ -46,15 +49,19 @@ class App(QWidget):
         self.show()
 
     def samrtTrans(self):
-        sender = self.sender()
-        sender.setDisabled(True)
-        result = self.tools.smartTrans()
-        sender.setDisabled(False)
+        result = False
+        rule = self.ruleText.toPlainText()
+        if len(rule) > 0:
+            result = self.transInput(rule)
+        if result == False:
+            sender = self.sender()
+            sender.setDisabled(True)
+            result = self.tools.smartTrans()
+            sender.setDisabled(False)
         if result == True:
             NotificationWindow.success('提示', '转换成功')
         else:
             NotificationWindow.error('提示', '转换失败')
-
 
     # 去除引号
     def filterQuotation(self):
@@ -70,3 +77,22 @@ class App(QWidget):
         # # msgBox.setText('转换成功')
         # msgBox.setStandardButtons(QMessageBox.Ok )
         # msgBox.button(QMessageBox.Ok).animateClick(3000)
+
+    def transInput(self, rule):
+        clipboard = QApplication.clipboard()
+        content = clipboard.text()
+
+        result = re.compile(r'(\$\d+)').findall(rule)
+        if len(result) > 0:
+            regexpRule = ""
+            for num in range(len(result)):
+                regexpRule = regexpRule + "(\S+)"
+                if num != len(result) - 1:
+                    regexpRule = regexpRule + "\s+"
+
+            rule = rule.replace('$', '\\')
+            newContent = re.sub(regexpRule, rule, content)
+            clipboard.setText(newContent)
+            return True
+
+        return False
--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from app import App
 from loguru import logger
 
-
 if __name__ == '__main__':
     logger.debug('........启动.......')
     app = App()
-
